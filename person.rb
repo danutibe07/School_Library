37ab34952256b6a854eb2224db048a577a518c11
danutibe07 <danielutibe07@gmail.com>
2023-05-24 03:00:21 +0100

    Correcting Linter errors

--- person.rb
+++ person.rb
@@ -39,5 +39,5 @@ puts person.correct_name
 capitalized_person = CapitalizeDecorator.new(person)
 puts capitalized_person.correct_name
 
-capitalized_trimmed_person = TrimmerDecorator.new(person)
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
 puts capitalized_trimmed_person.correct_name
